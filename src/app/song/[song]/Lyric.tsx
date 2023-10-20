5aaee98461c0684dc4e43940fafa2dcdb8cb7dcb
Ch3ru09 <xliang0905@gmail.com>
2023-10-19 23:11:32 -0400

    fixed lyrics for 夜に駆ける

--- src/app/song/[song]/Lyric.tsx
+++ src/app/song/[song]/Lyric.tsx
@@ -2,15 +2,14 @@ import styles from "./lyrics.module.css";
 
 export default function Lyric({ lyrics }: { lyrics: string[] }) {
   return (
-    <div>
-      {lyrics.map((line) => {
-        return <Line text={line} />;
+    <div className={styles["lines-container"]}>
+      {lyrics.map((line, i) => {
+        return <Line text={line} key={i} />;
       })}
     </div>
   );
 }
 
 function Line({ text }: { text: string }) {
-  return text == "" ? <br /> : <div className={styles["line"]}>{text}</div>;
+  return text ? <div className={styles["line"]}>{text}</div> : <br />;
 }
-
